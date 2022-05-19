604a4eaf2331eb748bce465eaa3c32beec0d829b
Ahmed Ismail <ahmed@Ahmeds-MacBook-Pro.local>
2022-05-19 21:04:06 +0100

    section and api from small cards

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import Banner from "../components/Banner"
 
 
 
-function Home() {
+function Home(props) {
+  props.exploreData
   return (
     <div>
       <Head>
@@ -15,9 +16,35 @@ function Home() {
       </Head>
       <Header />
       <Banner/>
-      <Smallcard/>
+<main className="max-w-7xl mx-auto px-8 sm:p-16 "> 
+<section className="pt-6">
+
+<h2 className="text-4xl font-semibold pb-5">
+  Explore Nearby
+</h2>
+
+
+{/* {pull api data} */}
+
+
+</section>
+
+
+</main>
+
+
     </div>
   )
 }  
 
 export default Home
+export async function getStaticProps (params) {
+const exploreData = await fetch ('https://links.papareact.com/pyp').then(res => res.json());
+
+return {
+  props:{
+    exploreData
+  }
+}
+
+}
